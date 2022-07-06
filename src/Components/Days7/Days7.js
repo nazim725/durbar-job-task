57d4da30f4a9e14168068347fdef0b88fd69af68
nazim725 <nazimhabib77@gmail.com>
2022-07-06 06:04:26 -0700

    added

--- src/Components/Days7/Days7.js
+++ src/Components/Days7/Days7.js
@@ -3,8 +3,8 @@ import './Days7.css'
 
 const Days7 = () => {
   return (
-    <div>
-        <h1 className="fs-1 fw-bold text-center mt-5 "> 7 days functionality will be rendered</h1>
+    <div className='days7'>
+        <h1 className="fs-1 fw-bold text-center "> 7 days functionality will be rendered</h1>
     </div>
   )
 }
